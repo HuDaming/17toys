fe6782aad8991b353b39611f9352285aaf4af932
Hudaming <40834106@qq.com>
2018-11-28 02:02:29 +0000

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,6 +6,31 @@ class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'group_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+        // 处理 N + 1 问题
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
