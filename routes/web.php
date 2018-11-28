1236b294067eae837fa648a6fc5c63e9fc770c92
Hudaming <40834106@qq.com>
2018-11-28 00:51:17 +0000

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-Route::resource('groups', 'GroupsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::resource('groups', 'GroupsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
